89876413d3701704240d37ec7d1a12041f992122
Chrys David <chrysdavidbr12@gmail.com>
2024-07-03 22:26:13 +0200

    Amélioration 2

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -33,3 +33,12 @@ function toggleMenu() {
         navbar.style.display = "flex";
     }
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
